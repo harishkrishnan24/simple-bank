6f4c1f597d92c64239e81761d19852c047166128
Harish Krishnan <harish.krishnan@digitalroute.com>
2025-05-26 18:12:16 +0200

    setup project and initialize sqlc

---
+++ db/query/account.sql
@@ -0,0 +1,2 @@
+-- name: CreateAccount :one
+INSERT into accounts (owner, balance, currency) VALUES ($1, $2, $3) RETURNING *;
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/harishkrishnan24/simple-bank
+
+go 1.23.4
